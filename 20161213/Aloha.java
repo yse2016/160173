adc2e2fc3b3f806710f098d03cab9cf87ee396a3
KyounoMayuu <kyouno_mayuu@yse-c.net>
2016-12-13 14:41:00 +0900

    Aloha&args -> Aloha&Scanner

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,14 +1,22 @@
 public class Aloha{
   public static void main(String[] args){
-    String mainMsg = args[0];
-    String subMsg = args[1];
+    // String Msg0 = args[0];
+    // String Msg1 = args[1];
 
-    for(int i=0; i<20; i++){
+    System.out.println("Type mainMsg.");
+    String mainMsg = new java.util.Scanner(System.in).nextLine();
+    System.out.println("Type subMsg.");
+    String subMsg  = new java.util.Scanner(System.in).nextLine();
+
+    System.out.println(mainMsg);
+    System.out.println(subMsg);
+
+    /* for(int i=0; i<20; i++){
       if( i%3 == 0 ){
 	System.out.println(mainMsg);
       } else {
         System.out.println(subMsg);
       }
-    }
+    } */
   }
 }
