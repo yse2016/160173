c2c70222c6f0af8ac536e8c491cd8f7116e3d361
KyounoMayuu <kyouno_mayuu@yse-c.net>
2016-12-13 14:28:29 +0900

    Aloha20 -> Aloha&Hello

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,7 +1,11 @@
 public class Aloha{
   public static void main(String[] args){
     for(int i=0; i<20; i++){
-      System.out.println("Aloha!");
+      if( i%3 == 0 ){
+	System.out.println("Hello!");
+      } else {
+        System.out.println("Aloha!");
+      }
     }
   }
 }
