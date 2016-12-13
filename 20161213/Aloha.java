eee2feb208bed0059a14e3aea461c949e5fc6b97
KyounoMayuu <kyouno_mayuu@yse-c.net>
2016-12-13 14:33:26 +0900

    Aloha&Hello -> Aloha&args

--- 20161213/Aloha.java
+++ 20161213/Aloha.java
@@ -1,10 +1,13 @@
 public class Aloha{
   public static void main(String[] args){
+    String mainMsg = args[0];
+    String subMsg = args[1];
+
     for(int i=0; i<20; i++){
       if( i%3 == 0 ){
-	System.out.println("Hello!");
+	System.out.println(mainMsg);
       } else {
-        System.out.println("Aloha!");
+        System.out.println(subMsg);
       }
     }
   }
